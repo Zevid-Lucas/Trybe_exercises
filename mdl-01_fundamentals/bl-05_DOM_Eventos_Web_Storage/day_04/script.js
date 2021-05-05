a1d9474900ee9f031b930022c3e156b723e40493
David Azevedo <zevid.dev@outlook.com>
2021-05-05 08:48:37 -0300

    Done question 2

--- mdl-01_fundamentals/bl-05_DOM_Eventos_Web_Storage/day_04/script.js
+++ mdl-01_fundamentals/bl-05_DOM_Eventos_Web_Storage/day_04/script.js
@@ -1,5 +1,7 @@
 let getColorInput = document.querySelector('#font-color');
+let getBgcInput = document.querySelector('#bgc-color');
 
+//EventListener color
 getColorInput.addEventListener('input', function (event) {
   let fontColor = event.target.value;
   document.querySelector('body').style.color = fontColor;
@@ -7,8 +9,19 @@ getColorInput.addEventListener('input', function (event) {
   localStorage.setItem('color', fontColor);
 });
 
+//EventListener backgroundColor
+getBgcInput.addEventListener('input', function (event) {
+  let bgcColor = event.target.value;
+  document.querySelector('body').style.backgroundColor = bgcColor;
+  localStorage.setItem('backgroundColor', bgcColor);
+});
+
+// Resgata as definições selecionadas
 function formatSelected() {
   let colorSelected = localStorage.getItem('color');
   document.querySelector('body').style.color = colorSelected;
+
+  let bgcSelected = localStorage.getItem('backgroundColor');
+  document.querySelector('body').style.backgroundColor = bgcSelected;
 }
 formatSelected();
