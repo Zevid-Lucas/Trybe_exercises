50519b44898501a0b2a13c2df96df1e1b37de089
David Azevedo <zevid.dev@outlook.com>
2021-06-26 17:13:41 -0300

    Create card pokemon with data each pokemon

--- mdl-02_front-end/bl-11_introducao_react/day_02/my-pokedex/src/Pokemon.js
+++ mdl-02_front-end/bl-11_introducao_react/day_02/my-pokedex/src/Pokemon.js
@@ -6,8 +6,9 @@ class Pokemon extends Component {
     return (
       <section>
         <h4>{pokemon.name}</h4>
-        <p>Elétrico</p>
-        <p>Peso: 6.0 Kg</p>
+        <p>{pokemon.type}</p>
+        <p>Peso: {pokemon.averageWeight.value} {pokemon.averageWeight.measurementUnit} </p>
+        <img src={pokemon.image} alt={pokemon.name} />
       </section>
     )
   }
