0f1ad072e2eadffb04cbec1c0e6ca1d87bac7862
David Azevedo <zevid.dev@outlook.com>
2021-06-26 16:52:49 -0300

    Create pokemon.js and export/render in pokedex.js

--- mdl-02_front-end/bl-11_introducao_react/day_02/my-pokedex/src/pokedex.js
+++ mdl-02_front-end/bl-11_introducao_react/day_02/my-pokedex/src/pokedex.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import Pokemon from './pokemon';
 
 class Pokedex extends Component {
   render() {
     return (
       <main>
-        <p>Component Pokedex</p>
+        <Pokemon />
       </main>
     )
   }
---
+++ mdl-02_front-end/bl-11_introducao_react/day_02/my-pokedex/src/pokemon.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Pokemon extends Component {
+  render() {
+    return (
+      <section>
+        <h4>Pikachu</h4>
+        <p>Elétrico</p>
+        <p>Peso: 6.0 Kg</p>
+      </section>
+    )
+  }
+}
+
+export default Pokemon;
