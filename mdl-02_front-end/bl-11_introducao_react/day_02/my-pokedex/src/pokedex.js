b73560f210ee1723f8b8c3ba478f6e3524262f66
David Azevedo <zevid.dev@outlook.com>
2021-06-26 16:48:48 -0300

    Create pokedex.js and export/render in App.js

---
+++ mdl-02_front-end/bl-11_introducao_react/day_02/my-pokedex/src/pokedex.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Pokedex extends Component {
+  render() {
+    return (
+      <main>
+        <p>Component Pokedex</p>
+      </main>
+    )
+  }
+}
+
+export default Pokedex;
