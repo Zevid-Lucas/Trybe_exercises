5f198d896b5654f3fac88fbb751193f3e8747826
zevid-lucas <zevid.dev@outlook.com>
2021-10-06 15:57:46 -0300

    done exercises 1..13, use update and yours operators

---
+++ mdl-03-back-end/block-23-updatesSimples_complexos/day_01/exercicio12.js
@@ -0,0 +1,13 @@
+// Exercício 12 : Utilizando uma única operação, remova os campos budget e estimatedBudget 
+// em todos os documentos.
+
+
+db.movies.updateMany(
+  {}, 
+  { $unset: { 
+      budget: '', 
+      estimatedBudget: ''
+    }
+  }
+)
+
---
+++ mdl-03-back-end/block-23-updatesSimples_complexos/day_01/exercicio9.js
@@ -0,0 +1,12 @@
+// Exercício 9 : Utilize o operador $max para alterar o imdbRating para 8.6 do filme
+// Godzilla . Além disso, altere a categoria "adventure" para "thriller" do filme Godzilla .
+
+
+db.movies.updateOne(
+  {title: "Godzilla"},
+  {
+   $set:{"category.1": "thriller"},
+   $max: {imdbRating: 8.6}
+  }
+)
+
